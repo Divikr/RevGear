fc65a760607912089a2c9a245a79f1e3e909268c
Divikr <divith2526@gmail.com>
2025-02-04 09:25:42 +0530

    edit5

--- config/passport.js
+++ config/passport.js
@@ -18,7 +18,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "https://revgear.site/auth/google/callback || http://localhost:4000/auth/google/callback",
+      callbackURL: "https://revgear.site/auth/google/callback" || "http://localhost:4000/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
